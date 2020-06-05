fc8f228162f6cc7769aa6f461faf50199a0f2afd
JeremYD <delpech.jeremy@hotmail.fr>
2020-06-05 17:09:16 +0200

    Boucle de base

--- src/fr/boucles/ExerciceBoucleBase.java
+++ src/fr/boucles/ExerciceBoucleBase.java
@@ -4,6 +4,13 @@ public class ExerciceBoucleBase {
 
 	public static void main(String[] args) {
 		
+	//Affichage de tous les nombres
+	int[] numberSimple = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	
+	for (int i = 0; i < numberSimple.length;  i++) {
+		System.out.println(numberSimple[i]);
+	}
+		
 	}
 	
 }
