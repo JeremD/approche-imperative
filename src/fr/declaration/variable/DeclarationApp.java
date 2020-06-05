31676ac2db69ddf916bce51de18c8e2bed16577f
JeremYD <delpech.jeremy@hotmail.fr>
2020-06-05 14:43:36 +0200

    Création variables

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -10,7 +10,20 @@ package fr.declaration.variable;
 public class DeclarationApp {
 
 	public static void main(String[] args) {
-		
+	
+	byte monByte = 3;
+	short monShort = 26;
+	int monInt = 632;
+	long monLong = 140000;
+	
+	float monFloat = 3;
+	double monDouble = 6.55555555555;
+	
+	char monChar = 'z';
+	String monString = "hello world!";
+	boolean monBool = true;
+	
+	System.out.println();
 	}
 
 }
