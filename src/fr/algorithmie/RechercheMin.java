96f4df105df0f4bc9cc52f37f2e3a72fb6fef815
JeremYD <delpech.jeremy@hotmail.fr>
2020-06-08 14:40:18 +0200

    Recherche Min

--- src/fr/algorithmie/RechercheMin.java
+++ src/fr/algorithmie/RechercheMin.java
@@ -7,7 +7,7 @@ package fr.algorithmie;
  * @author Jeremy
  *
  */
-public class RechercheMax {
+public class RechercheMin {
 
 	/**
 	 * @param args
@@ -17,17 +17,17 @@ public class RechercheMax {
 	// Tableau array
 	int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
 	
-	// Initialisation valeur max
-	int valueMax = array[0];
+	// Initialisation valeur min
+	int valueMin = array[0];
 	
-	// Recheche du plus grand élément du tableau
+	// Recheche du plus petit élément du tableau
 	for (int i = 0; i < array.length; i++) {
-		if (array[i] > valueMax) {
-			valueMax = array[i];
+		if (array[i] < valueMin) {
+			valueMin = array[i];
 		}
 	}
 	
-	System.out.println("Valeur la plus grande : " + valueMax);
+	System.out.println("Valeur la plus petite : " + valueMin);
 	
 	}
 
