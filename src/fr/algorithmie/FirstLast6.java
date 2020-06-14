318ba0cb89c777084efb2503d69750d8e1c999fc
JeremYD <delpech.jeremy@hotmail.fr>
2020-06-14 21:50:01 +0200

    Autoformation: First Last 6

---
+++ src/fr/algorithmie/FirstLast6.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package fr.algorithmie;
+
+/**
+ * 
+ * Exercices en auto-formation
+ * 
+ * EXERCICE First Last 6
+ * 
+ * @author Jeremy
+ *
+ */
+public class FirstLast6 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// Tableau d'entier
+		int[] arrayEntiers = { 5, 23, 4, 6 };
+
+		// Booléen d'analyse du tableau
+		boolean arrayBoolean;
+
+		// Vérification si tableau non vide, puis si premier élément ou dernier élément vaut 6
+		if (arrayEntiers != null && arrayEntiers[0] == 6 || arrayEntiers[arrayEntiers.length - 1] == 6) {
+			arrayBoolean = true;
+		} else {
+			arrayBoolean = false;
+		}
+
+		System.out.println(arrayBoolean);
+	}
+
+}
