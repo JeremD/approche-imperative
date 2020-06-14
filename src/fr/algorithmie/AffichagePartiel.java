001a79f5387635a53d7dcffefd9cfcbee500a55c
JeremYD <delpech.jeremy@hotmail.fr>
2020-06-14 12:11:27 +0200

    Autoformation: Affichage Partiel

--- src/fr/algorithmie/AffichagePartiel.java
+++ src/fr/algorithmie/AffichagePartiel.java
@@ -3,7 +3,8 @@ package fr.algorithmie;
 /**
  * Exercices en auto-formation
  * 
- * EXERCICE 3 AFFICHAGEPARTIEL
+ * EXERCICE Affichage Partiel
+ * 
  * @author Jeremy
  *
  */
@@ -13,30 +14,30 @@ public class AffichagePartiel {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		// Tableau array
-		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
-		
-		// Affichage des éléments > 3
-		System.out.println("Valeurs supérieurs à 3 :");
+		int[] array = { 1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4 };
+
+		// Affichage Ã©lÃ©ments > 3
+		System.out.println("Valeurs supÃ©rieurs Ã  3 :");
 		for (int i = 0; i < array.length; i++) {
 			if (array[i] > 3) {
 				System.out.println(array[i]);
 			}
 		}
 		System.out.println("\n");
-		
-		
-		// Affichage des éléments pairs
+
+		// Affichage Ã©lÃ©ments pairs
 		System.out.println("Valeurs pairs :");
 		for (int i = 0; i < array.length; i++) {
-			if (array[i] % 2 == 0) { //on trouve l'élement pair avec le modulo
+			// modulo pour chercher Ã©lÃ©ments pairs
+			if (array[i] % 2 == 0) {
 				System.out.println(array[i]);
 			}
 		}
 		System.out.println("\n");
-		
-		// Affichage des index pairs
+
+		// Affichage index pairs
 		System.out.println("Valeurs avec index pairs :");
 		for (int i = 0; i < array.length; i++) {
 			if (i % 2 == 0) {
@@ -44,16 +45,15 @@ public class AffichagePartiel {
 			}
 		}
 		System.out.println("\n");
-		
-		// Affichage des éléments impairs
+
+		// Affichage Ã©lÃ©ments impairs
 		System.out.println("Valeurs impairs :");
 		for (int i = 0; i < array.length; i++) {
 			if (array[i] % 2 != 0) {
 				System.out.println(array[i]);
 			}
 		}
-		System.out.println("\n");
-		
+
 	}
 
 }
