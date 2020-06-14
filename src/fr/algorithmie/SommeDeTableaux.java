d41749b25067e7388ed5fa01f619499dffd9e1a6
JeremYD <delpech.jeremy@hotmail.fr>
2020-06-14 12:17:33 +0200

    Autoformation: Somme de Tableaux

--- src/fr/algorithmie/SommeDeTableaux.java
+++ src/fr/algorithmie/SommeDeTableaux.java
@@ -3,34 +3,35 @@ package fr.algorithmie;
 /**
  * Exercices en auto-formation
  * 
- * EXERCICE 7 SOMMEDETABLEAUX
+ * EXERCICE Somme de Tableaux
+ * 
  * @author Jeremy
  *
  */
 public class SommeDeTableaux {
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	
+
 		// Premier tableau
-		int[] array1 = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
-		
+		int[] array1 = { 1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4 };
+
 		// Second tableau
-		int[] array2 = {-1, 12, 17, 14, 5, -9, 0, 18, -6, 0, 4, -13, 5, 7, -2, 8, -1};
-		
+		int[] array2 = { -1, 12, 17, 14, 5, -9, 0, 18, -6, 0, 4, -13, 5, 7, -2, 8, -1 };
+
 		// Somme des deux tableaux
 		int[] arrayTotal = new int[array1.length];
-		for (int i = 0; i < array1.length ; i++) {
+		for (int i = 0; i < array1.length; i++) {
 			arrayTotal[i] = array1[i] + array2[i];
 		}
-			
+
 		System.out.println("Somme des deux tableaux :");
 		for (int i = 0; i < arrayTotal.length; i++) {
 			System.out.println(arrayTotal[i]);
 		}
-		
+
 	}
-	
+
 }
