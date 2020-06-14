a56b7dea45e64180a2c7a66e532e61c3b3aa76f4
JeremYD <delpech.jeremy@hotmail.fr>
2020-06-14 12:16:02 +0200

    Autoformation: Calcul Moyenne

--- src/fr/algorithmie/CalculMoyenne.java
+++ src/fr/algorithmie/CalculMoyenne.java
@@ -3,7 +3,8 @@ package fr.algorithmie;
 /**
  * Exercices en auto-formation
  * 
- * EXERCICE 6 CALCULMOYENNE
+ * EXERCICE Calcul Moyenne
+ * 
  * @author Jeremy
  *
  */
@@ -13,21 +14,21 @@ public class CalculMoyenne {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	
+
 		// Tableau array
-		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
-			
-		// Addition du total des éléments
+		int[] array = { 1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4 };
+
+		// Addition total des Ã©lÃ©ments
 		int valueTotal = 0;
 		for (int i = 0; i < array.length; i++) {
 			valueTotal = valueTotal + array[i];
 		}
-		
-		// Calcul de la moyenne
+
+		// Calcul moyenne
 		int valueMoyenne = valueTotal / array.length;
-		
-		System.out.println("Valeur moyenne : " + valueMoyenne);
-	
+
+		System.out.println("Moyenne du tableau : " + valueMoyenne);
+
 	}
 
 }
