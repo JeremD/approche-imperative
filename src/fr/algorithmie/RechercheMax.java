89880f899ae2931b73fb5b3add4d175317f269ce
JeremYD <delpech.jeremy@hotmail.fr>
2020-06-14 12:13:48 +0200

    Autoformation: Recherche Max

--- src/fr/algorithmie/RechercheMax.java
+++ src/fr/algorithmie/RechercheMax.java
@@ -3,7 +3,8 @@ package fr.algorithmie;
 /**
  * Exercices en auto-formation
  * 
- * EXERCICE 4 RECHERCHEMAX
+ * EXERCICE Recherche Max
+ * 
  * @author Jeremy
  *
  */
@@ -13,22 +14,22 @@ public class RechercheMax {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	
+
 		// Tableau array
-		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
-		
+		int[] array = { 1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4 };
+
 		// Initialisation valeur max
 		int valueMax = array[0];
-		
-		// Recherche du plus grand élément du tableau
+
+		// Recherche du plus grand Ã©lÃ©ment du tableau
 		for (int i = 0; i < array.length; i++) {
 			if (array[i] > valueMax) {
 				valueMax = array[i];
 			}
 		}
-	
+
 		System.out.println("Valeur la plus grande : " + valueMax);
-	
+
 	}
 
 }
