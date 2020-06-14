aa9854e21c9473a8efc15f8b88a4c029fedad846
JeremYD <delpech.jeremy@hotmail.fr>
2020-06-14 12:08:09 +0200

    Autoformation: Inversion contenu

--- src/fr/algorithmie/InversionContenu.java
+++ src/fr/algorithmie/InversionContenu.java
@@ -3,7 +3,8 @@ package fr.algorithmie;
 /**
  * Exercices en auto-formation
  * 
- * EXERCICE 2 INVERSIONCONTENU
+ * EXERCICE Inversion contenu
+ * 
  * @author Jeremy
  *
  */
@@ -13,40 +14,37 @@ public class InversionContenu {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		/*
-		 * Copie de tableau inversé
-		*/
-		
+
 		// Tableau array source
-		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
-		
+		int[] array = { 1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4 };
+
 		// Tableau arrayCopy destination
 		int[] arrayCopy = new int[array.length];
-		
-		
-		/* Copie d'array vers arrayCopy avec inversion des données
-			i -> valeur d'array	|	j -> index d'arrayCopy
-		*/
-		for (int i = array.length-1, j = 0; i >= 0; i--, j++) {
-				arrayCopy[j] = array[i];
-		}
-		
-		
+
 		/*
-		 * Affichage des deux tableaux
+		 * Copie d'array vers arrayCopy avec inversion des donnÃ©es 
+		 * 
+		 * i -> contenu d'array 
+		 * j -> index d'arrayCopy
 		 */
-		System.out.println("Résultat du premier tableau :");
+		for (int i = array.length - 1, j = 0; i >= 0; i--, j++) {
+			arrayCopy[j] = array[i];
+		}
+
+		/*
+		 * Affichage des tableaux
+		 */
+		System.out.println("RÃ©sultat du premier tableau :");
 		for (int i = 0; i < array.length; i++) {
 			System.out.println(array[i]);
 		}
 		System.out.println("\n");
-		
-		System.out.println("Résultat du tableau copié :");
+
+		System.out.println("RÃ©sultat du tableau copiÃ© :");
 		for (int i = 0; i < arrayCopy.length; i++) {
 			System.out.println(arrayCopy[i]);
 		}
-		
+
 	}
 
 }
