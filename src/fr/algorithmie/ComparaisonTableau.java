9b5cf7d372fb30d02b82e8128c9ce5be729d11d4
JeremYD <delpech.jeremy@hotmail.fr>
2020-06-14 12:25:15 +0200

    Autoformation: Comparaison Tableau

--- src/fr/algorithmie/ComparaisonTableau.java
+++ src/fr/algorithmie/ComparaisonTableau.java
@@ -3,7 +3,8 @@ package fr.algorithmie;
 /**
  * Exercices en auto-formation
  * 
- * EXERCICE 9 COMPARAISONTABLEAU
+ * EXERCICE Comparaison Tableau
+ * 
  * @author Jeremy
  *
  */
@@ -13,28 +14,30 @@ public class ComparaisonTableau {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		// Premier tableau
-		int[] array1 = {1, 15, -3, 8, 7, 4, -2, 28, -1, 17, 2, 3, 0, 14, -4};
-		
+		int[] array1 = { 1, 15, -3, 8, 7, 4, -2, 28, -1, 17, 2, 3, 0, 14, -4 };
+
 		// Second tableau
-		int[] array2 = {3, -8, 17, 5, -1, 4, 0, 6, 2, 11, -5, -4, 8};
-		
+		int[] array2 = { 3, -8, 17, 5, -1, 4, 0, 6, 2, 11, -5, -4, 8 };
+
 		/*
-		 *  Comparaison des deux tableaux
+		 * Comparaison des deux tableaux
 		 */
+		// Variable stockant nombre d'éléments commun
 		int arrayCompare = 0;
-		
-		for (int i = 0; i < array1.length ; i++) {
-			// R�solution du conflit quand un tableau est plus petit que l'autre
+
+		for (int i = 0; i < array1.length; i++) {
+			// Gestion du cas quand un tableau plus petit que l'autre
 			int value1 = (i < array1.length) ? array1[i] : 0;
 			int value2 = (i < array2.length) ? array2[i] : 0;
-			
+
+			// Incrément d'arrayCompare à chaque éléments commun
 			if (value1 == value2) {
 				arrayCompare++;
 			}
 		}
-		System.out.println("Nombre d��l�ments en commun : " + arrayCompare);
+		System.out.println("Nombre(s) d'élément(s) en commun : " + arrayCompare);
 
 	}
 
