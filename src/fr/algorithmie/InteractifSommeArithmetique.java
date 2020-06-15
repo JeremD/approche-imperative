1f100bcaaa7723486ed76a51b6c86cde47cd4467
JeremYD <delpech.jeremy@hotmail.fr>
2020-06-15 02:19:16 +0200

    Autoformation: Interactif Somme Arithmetique

---
+++ src/fr/algorithmie/InteractifSommeArithmetique.java
@@ -0,0 +1,39 @@
+package fr.algorithmie;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * Exercices en auto-formation
+ * 
+ * EXERCICE Interactif Somme Arithmetique
+ * 
+ * @author Jeremy
+ *
+ */
+public class InteractifSommeArithmetique {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		/* Saisie d'un nombre au clavier */
+		Scanner scanner = new Scanner(System.in);
+		int choixNombre;
+		int sommeNombres = 0;
+		
+		/* Somme entre 1 et le nombre choisi */ 
+		System.out.println("Veuillez choisir un nombre");
+		choixNombre = scanner.nextInt();
+		
+		for (int i = 1; i <= 10; i++) {
+			sommeNombres = choixNombre + i;
+		}
+		
+		System.out.println(sommeNombres);
+		scanner.close();
+
+	}
+
+}
