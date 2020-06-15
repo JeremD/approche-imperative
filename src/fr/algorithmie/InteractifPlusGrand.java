d43540bc2269383b61c03c9c07113436be3b2dff
JeremYD <delpech.jeremy@hotmail.fr>
2020-06-15 11:16:02 +0200

    Autoformation: Interactif Plus Grand

---
+++ src/fr/algorithmie/InteractifPlusGrand.java
@@ -0,0 +1,42 @@
+package fr.algorithmie;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * Exercices en auto-formation
+ * 
+ * EXERCICE Interactif Plus Grand
+ * 
+ * @author Jeremy
+ *
+ */
+public class InteractifPlusGrand {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// Variable stockant valeur la plus grande
+		int plusgrandNombre = 0;
+
+		/*
+		 * Choix des 10 nombres
+		 */
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Veuillez choisir 10 nombres :");
+		int choixNombre;
+
+		// Saisie de chaque nombre
+		for (int i = 0; i < 10; i++) {
+			choixNombre = scanner.nextInt();
+
+			if (choixNombre > plusgrandNombre) {
+				plusgrandNombre = choixNombre;
+			}
+		}
+		System.out.println("Le plus grand nombres : " + plusgrandNombre);
+	}
+
+}
